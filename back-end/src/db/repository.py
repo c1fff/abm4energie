dddfb7723b3c98578cf9ace35343c33bcd28094e
damwead <mikhal3vsky@gmail.com>
2026-04-24 00:10:22 +0200

    updated file structure

--- back-end/src/db/repository.py
+++ back-end/src/db/repository.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-SURVEY_FILE = Path(__file__).resolve().parents[2] / "survey.json"
+SURVEY_FILE = Path(__file__).resolve().parents[3] / "data" / "raw" / "survey.json"
 
 with open(SURVEY_FILE, "r", encoding="utf-8") as f:
         data = json.load(f)
